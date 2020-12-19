89b9323c78a1242d44d50cf588f22286e3ca9c1a
Jony Talukdar <jonytalukdar30@gmail.com>
2020-12-19 09:24:38 -0800

    my first commit

---
+++ src/scripts/index.js
@@ -0,0 +1,56 @@
+import '../styles/index.scss';
+
+// var vs let vs const
+// const a = 12;
+// a = 23;
+
+// console.log(a);
+
+// const arr = [];
+
+// arr.push(12, 12);
+// console.log(arr);
+
+// var a = 12;
+
+// if (true) {
+//   var a = 20;
+//   console.log(a);
+// }
+
+// console.log(a);
+
+// let b = 12;
+
+// if (true) {
+//   let b = 20;
+//   console.log(b);
+// }
+
+// console.log(b);
+
+// function demo() {
+//   {
+//     let x = 12;
+//     {
+//       const x = 23;
+//       console.log(x);
+//     }
+//     x = 15;
+//     console.log(x);
+//   }
+// }
+
+// demo();
+
+// template strings
+
+// const name = 'JOney talukdar';
+// const age = 21;
+// const email = '@gmail.com';
+
+// const details = 'name : ' + name + 'age : ' + age + 'email : ' + email;
+// console.log(details);
+
+// const temDetails = `name : ${name} age : ${age} email : ${email}`;
+// console.log(temDetails);
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/scripts/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    open: true,
+    port: 1212,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties'],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: 'html-loader',
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
